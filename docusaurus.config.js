1195b68f6ec1f32acecb2cebf71b1b1fe6138663
Arda GUNSUREN <ardagunsuren@gmail.com>
2024-02-17 20:20:41 +0200

    Some changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'DevDumper',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'DevDumper',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -129,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} DevDumper, Powered By ArdaGnsrn.`,
       },
       prism: {
         theme: prismThemes.github,
